1ada8cfaaed710c12fe15ed2e0c71ef71e59848a
b9111426 <b9111426@gmail.coom>
2022-08-01 12:37:53 +0800

    修正banner input無法輸入

--- src/js/asset/banner.js
+++ src/js/asset/banner.js
@@ -6,8 +6,9 @@ export function renderBanner ($, bannerPic) {
     `<div class="swiper-slide">
       <img class="img-fluid " src="${item.url}" alt="">
     </div>`
-    $('.swiper-wrapper-1').html(str)
   })
+  $('.swiper-wrapper-1').html(str)
+
   const bannerContent = /* html */
   `<div class="banner-content">
       <h1 class="banner-title display-3 fw-bold">探索<span class="banner-title-deco">台灣之美</span><br>讓我們更親近這片土地</h1>
@@ -15,7 +16,7 @@ export function renderBanner ($, bannerPic) {
             Guide</span>
           </h3>
   <div class="banner-search input-group mt-6">
-    <input type="text" class="form-control ps-3 fs-5" placeholder="你想去哪裡? 請輸入關鍵字" aria-label="Recipient's username"
+    <input type="text" class="test-btn form-control ps-3 fs-5" placeholder="你想去哪裡? 請輸入關鍵字" aria-label="Recipient's username"
       aria-describedby="button-addon2">
     <button class="btn btn-primary" type="button" id="button-addon2"><i class="bi bi-search"></i><span
         class="ms-3 banner-search-btn">收尋</span></button>
@@ -23,6 +24,5 @@ export function renderBanner ($, bannerPic) {
 </div>
 <div class="banner-filter"></div>
 `
-
   $('#main').append(bannerContent)
 }
