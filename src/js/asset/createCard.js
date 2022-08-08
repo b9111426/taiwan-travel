2ea6f2320b1728d9c2819ce5fd814f36f6834123
b9111426 <b9111426@gmail.coom>
2022-08-08 18:38:03 +0800

    修正 swiper

--- src/js/asset/createCard.js
+++ src/js/asset/createCard.js
@@ -4,32 +4,32 @@ export function createSwiperCard (sceneData) {
     return item.Picture.PictureUrl1 !== undefined
   }) // 過濾沒有圖片資料
 
-  const str = ''
+  let str = ''
   filterPicData.forEach((item) => {
     let startTime = null
     let endTime = null
     startTime = item.StartTime.split('T')
     endTime = item.EndTime.split('T')
     const times = startTime[0].split('-').join('/') + ' - ' + endTime[0].split('-').join('/')
-    // str += /* html */`
-    // <div class="swiper-slide ">
-    //  <div class="card h-100 bg-gray-100">
-    //      <img class="object-fix w-100" src="${item.Picture.PictureUrl1}" class="card-img-top" alt="...">
-    //        <div class="card-body d-flex flex-column px-2">
-    //          <p class="text-secondary ">${times}</p>
-    //          <h5 class="card-text mt-2 mb-3">${item.ActivityName}</h5>
-    //            <div class="row mt-auto">
-    //            <p class="col-6 text-secondary"><i class="bi bi-geo-alt"></i>${item.City}</p>
-    //            <a class="col-6 text-end" href="javascript:;">詳細介紹
-    //            <i class="bi bi-chevron-right"></i></a>
-    //            </div>
-    //        </div>
-    //  </div>
-    // </div>
-    // `
+    str += /* html */`
+    <div class="swiper-slide ">
+      <div class="card h-100 bg-gray-100">
+          <img class="object-fix w-100" src="${item.Picture.PictureUrl1}" class="card-img-top" alt="...">
+            <div class="card-body d-flex flex-column px-2">
+              <p class="text-secondary ">${times}</p>
+              <h5 class="card-text mt-2 mb-3">${item.ActivityName}</h5>
+                <div class="row mt-auto">
+                <p class="col-6 text-secondary"><i class="bi bi-geo-alt"></i>${item.City}</p>
+                <a class="col-6 text-end" href="javascript:;">詳細介紹
+                <i class="bi bi-chevron-right"></i></a>
+                </div>
+            </div>
+      </div>
+    </div>
+    `
   })
 
-  // $('.swiper-wrapper').eq(1).html(str)
+  $('.swiper-wrapper').eq(1).html(str)
 }
 
 export function createCard (sceneData) {
