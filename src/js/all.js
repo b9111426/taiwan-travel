9ed8fd6fc78df8d684cd198db5c70eb5ce6fc36f
b9111426 <b9111426@gmail.coom>
2022-08-18 11:37:09 +0800

    修正token程序

--- src/js/all.js
+++ src/js/all.js
@@ -6,7 +6,7 @@ import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min'
 
 // resource
 import '../stylesheets/all.scss'
-import { getAuthorizationHeader } from './asset/getToken'
+import getToken from './asset/getToken'
 import { createSwiper } from './asset/swiper'
 import { getData, filterData } from './asset/getData'
 import { createSwiperCard, createCard } from './asset/createCard'
@@ -30,29 +30,42 @@ $(() => {
   $('#search').html(searchComponent)
   $('#popularTopics').html(popularTopics)
 
-  const token = document.cookie.replace(/(?:(?:^|.*;\s*)tdxToken\s*\=\s*([^;]*).*$)|^.*$/, '$1')// 從cookie取token
-  if (token === '') {
-    console.log('cookie沒有token')
-    getAuthorizationHeader()
+  // 判斷是否有token
+  if (getToken.getCookieToken() === '') {
+    const tokenPromise = getToken.getAuthorizationHeader()
+    tokenPromise.then(res => {
+      const { access_token, expires_in } = res.data
+      document.cookie = `tdxToken=${access_token};max-age=${expires_in}`// 將token存入cookie
+      renderCard(access_token)
+    })
+      .catch(err => {
+        console.log(err)
+      })
+  } else {
+    renderCard(getToken.getCookieToken())
   }
 
+  // 判斷分頁，渲染分頁
   const url = location()
   if (window.location.href === 'http://localhost:1234/') { searchFn.init() } else if (url === 'scenePage.html' || url === 'activityPage.html' || url === 'foodPage.html') {
     breadcrumbFn.init()
     popularTopicsFn.init()
-    searchFn.init(token)
+    searchFn.init(getToken.getCookieToken())
   }
 
-  const data1 = getData(token, 'Activity', 40)
-  const data2 = filterData(token, 'ScenicSpot', 30, 'DescriptionDetail', '熱門打卡')
-  const data3 = filterData(token, 'Restaurant', 30, 'Description', '老店')
-  Promise.all([data1, data2, data3]).then(res => {
-    createSwiperCard(res[0].data) // 創建swiper card dom元素
-    const str1 = createCard(res[1].data)
-    const str2 = createCard(res[2].data)
-    $('.hotPoint-content').html(str1)
-    $('.tastyFood-content').html(str2)
-  }).then(() => {
-    createSwiper()// 創建swiper實體
-  })
+  // 渲染首頁畫面
+  function renderCard (token) {
+    const data1 = getData(token, 'Activity', 40)
+    const data2 = filterData(token, 'ScenicSpot', 30, 'DescriptionDetail', '熱門打卡')
+    const data3 = filterData(token, 'Restaurant', 30, 'Description', '老店')
+    Promise.all([data1, data2, data3]).then(res => {
+      createSwiperCard(res[0].data) // 創建swiper card dom元素
+      const str1 = createCard(res[1].data)
+      const str2 = createCard(res[2].data)
+      $('.hotPoint-content').html(str1)
+      $('.tastyFood-content').html(str2)
+    }).then(() => {
+      createSwiper()// 創建swiper實體
+    })
+  }
 })
--- src/js/asset/getToken.js
+++ src/js/asset/getToken.js
@@ -2,38 +2,29 @@
 import Qs from 'qs'
 import axios from 'axios'
 
-const config = {
-  tdxClientId: 'YjkxMTE0MjYtMjQyZTkwNGEtNThiZS00NjI4',
-  tdxClientSecret: 'ZWFiYzJmNTItMzIyYi00MjUxLTliNDUtMWZmY2FkN2UxNjU3'
-}
 export default {
+  config: {
+    tdxClientId: 'YjkxMTE0MjYtMjQyZTkwNGEtNThiZS00NjI4',
+    tdxClientSecret: 'ZWFiYzJmNTItMzIyYi00MjUxLTliNDUtMWZmY2FkN2UxNjU3'
+  },
+  getCookieToken () {
+    return document.cookie.replace(/(?:(?:^|.*;\s*)tdxToken\s*\=\s*([^;]*).*$)|^.*$/, '$1')// 從cookie取token
+  },
+  getAuthorizationHeader () {
+    const parameter = {
+      grant_type: 'client_credentials',
+      client_id: Buffer.from(this.config.tdxClientId, 'base64'),
+      client_secret: Buffer.from(this.config.tdxClientSecret, 'base64')
+    }
+    const authUrl = 'https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token'
 
-}
-export function getAuthorizationHeader () {
-  console.log('ajax取token')
-  const parameter = {
-    grant_type: 'client_credentials',
-    client_id: Buffer.from(config.tdxClientId, 'base64'),
-    client_secret: Buffer.from(config.tdxClientSecret, 'base64')
+    const options = {
+      method: 'POST',
+      url: authUrl,
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      data: Qs.stringify(parameter)
+    }
+    return axios(options)
   }
-  const authUrl = 'https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token'
 
-  const options = {
-    method: 'POST',
-    url: authUrl,
-    headers: { 'content-type': 'application/x-www-form-urlencoded' },
-    data: Qs.stringify(parameter)
-  }
-  axios(options)
-    .then(res => {
-      console.log('token取得成功')
-      const accessToken = res.data
-      const { access_token, expires_in } = accessToken
-      document.cookie = `tdxToken=${access_token};max-age=${expires_in}`// 將token存入cookie
-      return access_token
-    })
-    .catch(err => {
-      console.log('取token失敗')
-      console.log(err)
-    })
 }
