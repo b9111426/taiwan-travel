4312e56b9051f079562c274c24407ccd6bbc39d7
gary.shine <b9111426@gmail.com>
2022-09-05 15:19:59 +0800

    修正pagination

--- src/js/components/pagination copy.js
+++ src/js/components/pagination copy.js
@@ -9,15 +9,11 @@ export function renderPages (data) {
     allData: []
   }
   pageData.allData = data
-
-  //總頁籤數
   const rows = Math.ceil(pageData.allData.length / pageData.pageItems)
-
-  //創建過度按鈕
+  //創建按鈕
   const transition = $('<li/>', { class: 'page-item transition-item-js' }).append($('<a/>', { class: 'page-link link-secondary disabled', href: 'javascript:;', text: '...' }))
   const transition2 = transition.clone()
 
-//創建第1頁和最後頁按鈕
   const firstLink = $('<a/>', { class: 'page-link link-secondary', href: 'javascript:;', text: 1 })
   const firstItem = $('<li/>', { class: 'page-item firstPage-js' }).append(firstLink)
   $('.pagination').children('.page-item').eq(0).after(firstItem)
@@ -26,7 +22,6 @@ export function renderPages (data) {
   const lastItem = $('<li/>', { class: 'page-item  lastPage-js' }).append(lastALink)
   $('.pagination').children('.page-item').last().before(lastItem)
 
-//如果只有5個頁籤
   if (rows <= 5) {
     for (let idx = 1; idx < rows - 1; idx++) {
       const aLink = $('<a/>', { class: 'page-link link-secondary', href: 'javascript:;', text: idx + 1 })
@@ -35,18 +30,24 @@ export function renderPages (data) {
     }
   }
 
-//如果只有1頁籤或沒有資料
+  $('.pagination').find('.firstPage-js,.pages-js,.lastPage-js').on('click', function (e) {
+    e.preventDefault()
+    pageData.currentPage = +$(this).text()
+    renderCard()
+    if (rows > 5) { reRenderPages() }
+    changeClass()
+  })
+
   if (rows === 1 || rows === 0) {
     $('.lastPage-js').remove()
   }
 
-  // 渲染中間頁籤
+  // 渲染分頁
   function reRenderPages () {
     $('.firstPage-js').after(transition)
     $('.lastPage-js').before(transition2)
 
     if (pageData.currentPage <= 4) {
-      pageData.range = []
       $('.pagination').find('.pages-js').remove()
       $('.transition-item-js').eq(0).hide()
       $('.transition-item-js').eq(1).show()
@@ -59,7 +60,6 @@ export function renderPages (data) {
     } else if (pageData.currentPage > 4 && pageData.currentPage < rows - 3) {
       $('.transition-item-js').show()
       const isRange = pageData.range.includes(pageData.currentPage)
-      //判斷是否需重新渲染
       if (!isRange) {
         pageData.range = []
         $('.pagination').find('.pages-js').remove()
@@ -78,7 +78,6 @@ export function renderPages (data) {
       $('.transition-item-js').eq(1).hide()
       $('.transition-item-js').eq(0).show()
       $('.pagination').find('.pages-js').remove()
-      pageData.range = []
       for (let idx = 3; idx > 0; idx--) {
         const aLink = $('<a/>', { class: 'page-link link-secondary', href: 'javascript:;', text: rows - idx })
         const item = $('<li/>', { class: 'page-item pages-js' }).append(aLink)
@@ -106,7 +105,7 @@ export function renderPages (data) {
     cardEvent()
   }
 
-  // 頁籤交互樣式
+  // 替換樣式
   function changeClass () {
     const select = $('.page-item').filter(function () {
       return +$(this).text() === pageData.currentPage
@@ -126,32 +125,27 @@ export function renderPages (data) {
     }
   }
 
-  // 綁監聽
-  $('.pagination').find('.firstPage-js,.pages-js,.lastPage-js').on('click', function (e) {
-    pageData.currentPage = +$(this).text()
-    renderCard()
-    if (rows > 5) { reRenderPages() }
-    changeClass()
-  })
-
+  // 前後頁綁監聽
   $('.pageSub-js').on('click', function (e) {
+    e.preventDefault()
     if (pageData.currentPage > 1) {
       pageData.currentPage--
-      if (rows > 5) { reRenderPages() }
+      if (rows > 6) { reRenderPages() }
       renderCard()
       changeClass()
     }
   })
   $('.pageAdd-js').on('click', function (e) {
+    e.preventDefault()
     if (pageData.currentPage < rows) {
       pageData.currentPage++
-      if (rows > 5) { reRenderPages() }
+      if (rows > 6) { reRenderPages() }
       renderCard()
       changeClass()
     }
   })
 
   renderCard()
-  if (rows > 5) { reRenderPages() }
+  if (rows > 6) { reRenderPages() }
   changeClass()
 }
